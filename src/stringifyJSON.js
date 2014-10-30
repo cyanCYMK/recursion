63fffa12035dbfa1cc443c910fd196a5fc688557
Christine <cylee4@gmail.com>
2014-10-29 21:49:18 -0500

    Implement stringifyJSON for primitive types.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,14 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  var quote = '"';
+  if (typeof obj === undefined) {
+  	return obj;
+  } else if (!obj && obj !== 0) {
+  	return obj + '';
+  } else if (typeof obj === 'string') {
+  	return quote + obj + quote;
+  } else {
+  	return obj.toString();
+  }
 };
