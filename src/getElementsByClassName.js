b8f1f6566c830188ac2572447e8fd60b0f89d064
Christine <cylee4@gmail.com>
2014-11-05 23:21:45 -0600

    Able to return matching class in top level classList only. WIP

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,20 +8,45 @@ var getElementsByClassName = function(className){
   // your code here
   var body = document.body;
   var accumulator = (typeof accumulator === 'undefined') ? [] : accumulator;
+/*  var bodyClasses = body.classList || [];
+  var bodyIndex = bodyClasses.indexOf(className);
+  if (bodyClasses.indexOf(className) > -1) {
+  	
+  }
+*/
+  return getChildNodes(body, accumulator);
 
   // input element and looks through children until there are no more
   function getChildNodes(element, accumulator){
-  	elChildren = element.childNodes
+  	var elClasses = element.classList;
+  	var elChildren = Array.prototype.slice.apply(element.childNodes);
+    if (typeof elClasses !== 'undefined') {
+    	// loop through elClasses and look if it contains className
+    	// if it contains then push it
+    	elClasses = Array.prototype.slice.apply(elClasses);
+    	elClasses.forEach(function(val) {
+          if (val === className) {
+          	accumulator.push(element);
+          }
+    	});
+    	return accumulator;
+    }
+/*  	if (typeof elClasses !== 'undefined' && elClasses.contains(className)) {
+      accumulator.push(element);
+  	}*/
   	// if children has more children in it, do until childNodes is []
-  	if (elChildren.length === 0) {
+  	/*if (elChildren.length === 0) {
   		return accumulator;
   	}
     for (var i=0, l=elChildren.length; i<l; i++) {
-  	   if (elChildren[i].classList.contains(className)) {
+      var childClasses = elChildren[i].classList;
+  	  if (typeof childClasses === 'undefined' || childClasses.length === 0) {
+  	    continue;
+  	  }
+  	  else if (childClasses.contains(className)) {
   	   	accumulator.push(elChildren[i]);
-  	   }
-    }
-
-  } 
+  	  }
+    }*/
+  }
 
 };
